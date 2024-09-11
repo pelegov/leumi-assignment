1c38c92f0c6e636a8fb9d95f730ff9c76fedb96f
pelegov <nave.peleg@gmail.com>
2024-09-11 16:10:17 +0300

    changes

--- ci-cd/flask/app.py
+++ ci-cd/flask/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify
-    
 
 app = Flask(__name__)
 
@@ -7,5 +6,5 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
-app.run(host='0.0.0.0', port=5001)
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
